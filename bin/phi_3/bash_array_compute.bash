0795e8c3d2631e38b414b7ddb69fd251e005c10d
Angus Chun-Kei Leung (Monash University) <aleung@m3-login2.massive.org.au>
2018-07-31 13:37:21 +1000

    sleeping 60s results in the times where queue is empty, changed to 30s

--- bin/phi_3/bash_array_compute.bash
+++ bin/phi_3/bash_array_compute.bash
@@ -12,7 +12,7 @@ for (( line=1; line<=$lines; line=$line+$line_increment )); do
 	echo "there are $jobs jobs"
 	while [ $jobs -ge 2 ]; do
 		echo "too many jobs, sleeping"
-		sleep 60s
+		sleep 30s
 		squeue -u aleung > job_list
 		jobs=$(wc -l < job_list)
 		echo "slept, now there are $jobs jobs"
@@ -21,5 +21,5 @@ for (( line=1; line<=$lines; line=$line+$line_increment )); do
 	echo "array submitting (from line $line)"
 	sbatch --job-name="phi3_array" --output="logs/array_%A_%a.out" --error="logs/array_%A_%a.err" --array=1-$line_increment bash_array_sbatch.bash $line
 	echo "submitted, now sleeping for a little"
-	sleep 60s
+	sleep 30s
 done
