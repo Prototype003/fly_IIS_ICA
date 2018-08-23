47bc97757f9ecfcc73bf8012adf8dfd1113bb840
Angus Leung <prototype032@gmail.com>
2018-08-23 17:00:51 +1000

    Correlation plots

--- bin/phi_3/main_composition_correlation.m
+++ bin/phi_3/main_composition_correlation.m
@@ -10,30 +10,31 @@ For seeing if there's a relationship between two-channel phi and their
 %% Setup
 
 nChannels = [2 4];
+tau = 1; % 1=4ms
 
 %% Load relevant files
 
-% global_tpm = 0;
-% 
-% source_dir = 'results/';
-% 
-% phis = cell(4, 1);
-% 
-% % 2 channels
-% source_2ch = ['split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim_phithree_nChannels2_globalTPM' num2str(global_tpm) '.mat'];
-% tmp = load([source_dir source_2ch]);
-% phis{2} = tmp.phis{1};
-% phis{2}.concept_list_full = concept_list(2); % Relevant 2nd order concept is the 3rd one
-% 
-% % 4 channels
-% tic
-% disp('loading 4ch');
-% source_4ch = ['split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim_phithree_nChannels4_globalTPM' num2str(global_tpm) '.mat'];
-% tmp = load([source_dir source_4ch]);
-% phis{4} = tmp.phis{1};
-% disp('loaded 4ch');
-% phis{4}.concept_list_full = concept_list(4);
-% toc
+global_tpm = 0;
+
+source_dir = 'results/';
+
+phis = cell(4, 1);
+
+% 2 channels
+source_2ch = ['split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim_phithree_nChannels2_globalTPM' num2str(global_tpm) '.mat'];
+tmp = load([source_dir source_2ch]);
+phis{2} = tmp.phis{1};
+phis{2}.concept_list_full = concept_list(2); % Relevant 2nd order concept is the 3rd one
+
+% 4 channels
+tic
+disp('loading 4ch');
+source_4ch = ['split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim_phithree_nChannels4_globalTPM' num2str(global_tpm) '.mat'];
+tmp = load([source_dir source_4ch]);
+phis{4} = tmp.phis{1};
+disp('loaded 4ch');
+phis{4}.concept_list_full = concept_list(4);
+toc
 
 %% Compare second-order concepts
 
@@ -51,6 +52,9 @@ end
 
 % Storage structure
 tmp = size(phis{4}.big_mips);
+if length(tmp) < 8
+    tmp(8) = 1;
+end
 storage_size = [matches tmp([5 6 7 8 2])];
 concept_phi = struct();
 concept_phi.twoCh = zeros(storage_size);
@@ -75,17 +79,19 @@ for channel_pair_counter = 1 : size(phis{2}.channel_sets, 1)
             channel = channel_pair(channel_counter);
             channel_pair_location(channel_counter) = find(phis{4}.channel_sets(superset, :) == channel);
         end
-        
+        %channel_pair_location = channel_pair_location(2); Can compare first-order concepts as well
         % Get small-phi corresponding to the relevant concept
         for concept = 1 : length(phis{4}.concept_list_full)
             if isequal(phis{4}.concept_list_full{concept}, channel_pair_location)
+                concept_2ch = 3; % The 2nd order concept in 2ch is always the third one (the last one)
+                
                 % 2ch - get weighted average of small phi across states
-                phi_2ch = permute(phis{2}.big_mips(:, :, 3, channel_pair_counter, :, :, :, :), [1 5 6 7 8 2 3 4]);
+                phi_2ch = permute(phis{2}.big_mips(:, :, concept_2ch, channel_pair_counter, :, :, :, :), [1 5 6 7 8 2 3 4]);
                 phi_2ch_weighted = phi_2ch;
                 phi_2ch_weighted(:, :, :, :, :, 1) = phi_2ch_weighted(:, :, :, :, :, 1) .* single(permute(phis{2}.state_counters(:, channel_pair_counter, :, :, :, :), [1 3 4 5 6 2]));
                 phi_2ch_weighted(:, :, :, :, :, 2) = phi_2ch_weighted(:, :, :, :, :, 2) .* single(permute(phis{2}.state_counters(:, channel_pair_counter, :, :, :, :), [1 3 4 5 6 2]));
                 phi_2ch_summed = permute(sum(phi_2ch_weighted, 1), [2 3 4 5 6 1]); % Sum across states
-                phi_2ch_mean = phi_2ch_summed / sum(phis{2}.state_counters(:, 1, 1, 1, 1, 4)); % Assumes all parameters have the same total number of states
+                phi_2ch_mean = phi_2ch_summed / sum(phis{2}.state_counters(:, 1, 1, 1, 1, tau)); % Assumes all parameters have the same total number of states
                 concept_phi.twoCh(match_counter, :, :, :, :, :) = phi_2ch_mean;
                 
                 % 4ch - get weighted average of small phi across states
@@ -94,7 +100,7 @@ for channel_pair_counter = 1 : size(phis{2}.channel_sets, 1)
                 phi_4ch_weighted(:, :, :, :, :, 1) = phi_4ch_weighted(:, :, :, :, :, 1) .* single(permute(phis{4}.state_counters(:, superset, :, :, :, :), [1 3 4 5 6 2]));
                 phi_4ch_weighted(:, :, :, :, :, 2) = phi_4ch_weighted(:, :, :, :, :, 2) .* single(permute(phis{4}.state_counters(:, superset, :, :, :, :), [1 3 4 5 6 2]));
                 phi_4ch_summed = permute(sum(phi_4ch_weighted, 1), [2 3 4 5 6 1]); % Sum across states
-                phi_4ch_mean = phi_4ch_summed / sum(phis{4}.state_counters(:, 1, 1, 1, 1, 4)); % Assumes all parameters have the same total number of states
+                phi_4ch_mean = phi_4ch_summed / sum(phis{4}.state_counters(:, 1, 1, 1, 1, tau)); % Assumes all parameters have the same total number of states
                 concept_phi.fourCh(match_counter, :, :, :, :, :) = phi_4ch_mean;
                 
                 break;
@@ -110,8 +116,59 @@ end
 
 flies = (1:13);
 
+% figure;
+% %scatter(concept_phi.twoCh(:), concept_phi.fourCh(:));
+% x_vals = squeeze(mean(mean(concept_phi.twoCh(:, :, flies, 1, tau, 1), 2), 3));
+% y_vals = squeeze(mean(mean(concept_phi.fourCh(:, :, flies, 1, tau, 1), 2), 3));
+% scatter(x_vals, y_vals);
+
+% Plot for each fly
 figure;
-%scatter(concept_phi.twoCh(:), concept_phi.fourCh(:));
-x_vals = squeeze(mean(mean(concept_phi.twoCh(:, :, flies, 1, 4, 1), 2), 3));
-y_vals = squeeze(mean(mean(concept_phi.fourCh(:, :, flies, 1, 4, 1), 2), 3));
-scatter(x_vals, y_vals);
+condition = 1;
+for fly = 1 : 13
+    subplot(3, 6, fly);
+    x_vals = squeeze(mean(mean(concept_phi.twoCh(:, :, fly, condition, tau, 1), 2), 3));
+    y_vals = squeeze(mean(mean(concept_phi.fourCh(:, :, fly, condition, tau, 1), 2), 3));
+    scatter(x_vals, y_vals);
+    corr(x_vals, y_vals)
+    
+    axis equal;
+    
+    xlabel('2ch concept \phi'); ylabel('4ch concept \phi');
+end
+
+%%
+% Plot across flies
+figure;
+partition_titles = {'unpartitioned', 'partitioned'};
+condition_colours = 'rb';
+condition_shapes = 'ox';
+for partitioned = 1 : 2
+    subplot(1, size(concept_phi.twoCh, 4), partitioned);
+    for condition = 1 : size(concept_phi.twoCh, 4)
+        x_vals = squeeze(mean(mean(concept_phi.twoCh(:, :, fly, condition, tau, partitioned), 2), 3));
+        y_vals = squeeze(mean(mean(concept_phi.fourCh(:, :, fly, condition, tau, partitioned), 2), 3));
+        scatter(x_vals, y_vals, [condition_colours(condition) condition_shapes(condition)]); hold on;
+        [r, p] = corr(x_vals, y_vals)
+    end
+    title(partition_titles{partitioned});
+    %axis equal;
+    
+    xlabel('2ch concept \phi'); ylabel('4ch concept \phi');
+end
+
+%%
+% Plot across flies
+figure;
+partition_titles = {'unpartitioned', 'partitioned'};
+for partitioned = 1 : 2
+    subplot(1, size(concept_phi.twoCh, 4), partitioned);
+    x_vals = squeeze(mean(mean(concept_phi.twoCh(:, :, fly, 1, tau, partitioned)-concept_phi.twoCh(:, :, fly, 2, tau, partitioned), 2), 3));
+    y_vals = squeeze(mean(mean(concept_phi.fourCh(:, :, fly, 1, tau, partitioned)-concept_phi.fourCh(:, :, fly, 2, tau, partitioned), 2), 3));
+    scatter(x_vals, y_vals, 'k.'); hold on;
+    [r, p] = corr(x_vals, y_vals)
+    title(partition_titles{partitioned});
+    %axis equal;
+    
+    xlabel('2ch concept \phi'); ylabel('4ch concept \phi');
+end
