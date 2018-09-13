8cdc5a40055be1d48dc1834f13d1af52dc5b5a32
Angus Leung <prototype032@gmail.com>
2018-09-13 10:04:14 +1000

    added plots for individual channel sets, and for mean+std across sets

--- bin/phi_3/main_tau_compare.m
+++ bin/phi_3/main_tau_compare.m
@@ -12,18 +12,23 @@ for N flies, Y channel sets, etc
 nChannels = 4;
 flies = (1:13);
 conditions = (1:2);
-%taus = 2.^(0:9);
-taus = 2.^(0:6);
+taus = 2.^(0:9);
+%taus = 2.^(0:6);
 
 nchoosek(15, nChannels);
 sets = floor(linspace(1, nchoosek(15, nChannels), 10));
 
 trials = (1);
-tau_offset = 0;
+binOffsets = 0;
 
 global_tpm = 1;
 tau_string = 'tau'; % 'tauBin' or 'tau'
 
+% 'tmp/' = results without any correction
+% 'tmp2/' = results with TPM 'normalisation' by penalising transition
+%   probabilities with less than n observations
+% 'tmp3/' = results with 'fairer' TPMs, built by offsetting samples before
+%   binning into coarse grained samples
 source_dir = 'tmp/';
 source_prefix = ['split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim'...
     '_nChannels' num2str(nChannels)...
@@ -43,16 +48,27 @@ source_prefix = ['split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim'.
 %             for tau_counter = 1 : length(taus)
 %                 for trial = trials
 %                     
-%                     % File name
+%                     % File name (for tmp3/)
 %                     source_file = [source_prefix...
 %                         '_f' sprintf('%02d', fly)...
 %                         'c' num2str(condition)...
 %                         tau_string num2str(taus(tau_counter))...
-%                         'tauOffset' num2str(tau_offset)...
+%                         'binOffsets' num2str(binOffsets)...
 %                         's' sprintf('%04d', network)...
 %                         't' num2str(trial)...
 %                         '.mat'...
 %                         ];
+%                     
+% %                     % File name (for tmp/ and tmp2)
+% %                     source_file = [source_prefix...
+% %                         '_f' sprintf('%02d', fly)...
+% %                         'c' num2str(condition)...
+% %                         tau_string num2str(taus(tau_counter))...
+% %                         'tauOffset' num2str(0)...
+% %                         's' sprintf('%04d', network)...
+% %                         't' num2str(trial)...
+% %                         '.mat'...
+% %                         ];
 %                     
 %                     try
 %                         % Load file
@@ -73,35 +89,83 @@ source_prefix = ['split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim'.
 % 
 % %% Save (so we don't need to keep loading)
 % 
-% save(['tmp/' tau_string '.mat'], 'phi_values');
+% save([source_dir tau_string '.mat'], 'phi_values');
 
 %% Load the saved one instead of all the separate files
 
-load(['tmp/' tau_string '.mat']);
+load([source_dir tau_string '.mat']);
 
 %% Plot phi averaged across sets, flies
 
-phi_values = phi_values(:, :, :, 1:length(taus));
+%phi_values = phi_values(:, :, :, 1:length(taus));
 
-flies = (11);
-condition = 1;
+flies = (1:13);
+
+% figure;
+% imagesc(squeeze(mean(phi_values(:, flies, 1, :), 2)));
+% %imagesc((squeeze(mean(phi_values(:, flies, 1, :), 2)) - squeeze(mean(phi_values(:, flies, 2, :), 2))) ./ squeeze(mean(phi_values(:, flies, 1, :), 2)));
+% 
+
+tau_log = log2(taus);
 
 figure;
-imagesc(squeeze(mean(phi_values(:, flies, 1, :), 2)));
-%imagesc((squeeze(mean(phi_values(:, flies, 1, :), 2)) - squeeze(mean(phi_values(:, flies, 2, :), 2))) ./ squeeze(mean(phi_values(:, flies, 1, :), 2)));
 
-
-figure;
+% Plot for each individual channel set
 for set_counter = 1 : length(sets)
-    plot(taus, squeeze(mean(phi_values(set_counter, flies, condition, :), 2))); hold on;
+    
+    subplot(2, 3, 1);
+    plot(tau_log, squeeze(mean(phi_values(set_counter, flies, 1, :), 2))); hold on;
+    title('wake');
+    ylabel('\Phi');
+    xlabel('log2(\tau)');
+    xlim([tau_log(1) tau_log(end)]);
+    ax = gca;
+    ax.YAxis.Exponent = 0;
+    yl = ylim;
+    
+    subplot(2, 3, 2);
+    plot(tau_log, squeeze(mean(phi_values(set_counter, flies, 2, :), 2))); hold on;
+    title('anest');
+    xlim([tau_log(1) tau_log(end)]);
+    ylim(yl);
+    ax = gca;
+    ax.YAxis.Exponent = 0;
+    
+    subplot(2, 3, 3);
+    plot(tau_log, squeeze(mean(phi_values(set_counter, flies, 1, :), 2)) - squeeze(mean(phi_values(set_counter, flies, 2, :), 2))); hold on;
+    title('wake-anest');
+    xlim([tau_log(1) tau_log(end)]);
+    ax = gca;
+    ax.YAxis.Exponent = 0;
 end
 
-figure;
-for set_counter = 1 : length(sets)
-    plot(taus, squeeze(mean(phi_values(set_counter, flies, 1, :), 2)) - squeeze(mean(phi_values(set_counter, flies, 2, :), 2))); hold on;
-end
+% Mean and std across the channel sets
+subplot(2, 3, 4);
+errorbar(tau_log, squeeze(mean(mean(phi_values(:, flies, 1, :), 2), 1)), squeeze(std(mean(phi_values(:, flies, 1, :), 2), [], 1))); hold on;
+title('wake');
+ylabel('\Phi');
+xlabel('log2(\tau)');
+xlim([tau_log(1)-1 tau_log(end)+1]);
+ax = gca;
+ax.YAxis.Exponent = 0;
+yl = ylim;
 
-legend('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
+subplot(2, 3, 5);
+errorbar(tau_log, squeeze(mean(mean(phi_values(:, flies, 2, :), 2), 1)), squeeze(std(mean(phi_values(:, flies, 2, :), 2), [], 1))); hold on;
+title('anest');
+xlim([tau_log(1)-1 tau_log(end)+1]);
+ax = gca;
+ax.YAxis.Exponent = 0;
+ylim(yl);
+
+subplot(2, 3, 6);
+errorbar(tau_log, squeeze(mean(mean(phi_values(:, flies, 1, :)-phi_values(:, flies, 2, :), 2), 1)), squeeze(std(mean(phi_values(:, flies, 1, :)-phi_values(:, flies, 2, :), 2), [], 1))); hold on;
+title('wake-anest');
+xlim([tau_log(1)-1 tau_log(end)+1]);
+ax = gca;
+ax.YAxis.Exponent = 0;
+
+%legend('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
 %% With variance across 2250ms trials
 
 phi_values = zeros(length(taus), length(trials));
