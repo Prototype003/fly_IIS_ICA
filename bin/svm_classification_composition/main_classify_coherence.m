5e262f2b3ba12101b2857a9ccd4fecc8df8e5468
Angus Leung <prototype032@gmail.com>
2019-02-12 11:56:19 +1100

    wrong dimension input

--- bin/svm_classification_composition/main_classify_coherence.m
+++ bin/svm_classification_composition/main_classify_coherence.m
@@ -60,7 +60,7 @@ results_file = ['coherence_svm_' class_type '.mat'];
 
 cost_powers = (-20:20);%0;%(-20:20);
 costs = 2 .^ cost_powers;
-cost_accuracies = zeros(size(costs), size(networks, 1));
+cost_accuracies = zeros(length(costs), size(networks, 1));
 
 % These create a parallel pool
 coherencies_par = parallel.pool.Constant(coherencies);
