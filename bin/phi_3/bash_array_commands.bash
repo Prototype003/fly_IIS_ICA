e15a67cba925ced8644e416c69b630d73508566b
Angus Leung <prototype032@gmail.com>
2019-03-14 16:41:45 +1100

    updated to compute for 3ch

--- bin/phi_3/bash_array_commands.bash
+++ bin/phi_3/bash_array_commands.bash
@@ -2,7 +2,7 @@
 nChannels=4
 flies=13 # 13
 conditions=(1 2)
-set_ids=($(seq 1 1 1365)) # ($(seq first step last)) # (1036)
+set_ids=($(seq 1 1 455)) # ($(seq first step last)) # (1036)
 taus=(4) # (1 2 3 4 8 12 16 24 32 48 64 128 256 512 4500 9000) #(4 8 16)
 trials=8
 global_tpm=0
