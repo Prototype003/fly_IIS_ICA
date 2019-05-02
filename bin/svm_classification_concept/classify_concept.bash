32d58103ed3ca85dfb095046dc1ce09351ea3e75
Angus Leung <prototype032@gmail.com>
2019-05-02 12:33:30 +1000

    classification using single concepts at a time

---
+++ bin/svm_classification_concept/classify_concept.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+declare -a types=("part" "unpart" "both")
+
+for type in "${types[@]}"; do
+	command="time matlab -nodisplay -nosplash -nodesktop -r \"main_classify_concept(${type}); exit\""
+	sbatch --job-name="${type}" --output="logs/${type}.out" --error="logs/${type}.err" classify_concept_sbatch.bash $command
+done
