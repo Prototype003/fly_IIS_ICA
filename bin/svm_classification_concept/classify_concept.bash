1a1c0c12aa79065ffa3a2528c676aa5ff19d18bf
Angus Leung <prototype032@gmail.com>
2019-05-02 13:08:25 +1000

    fixed no quotes (resulting in string with spaces being read as multiple arguments

--- bin/svm_classification_concept/classify_concept.bash
+++ bin/svm_classification_concept/classify_concept.bash
@@ -4,5 +4,5 @@ declare -a types=("part" "unpart" "both")
 
 for type in "${types[@]}"; do
 	command="matlab -nodisplay -nosplash -nodesktop -r \"main_classify_concept(${type}); exit\""
-	sbatch --job-name="${type}" --output="logs/${type}.out" --error="logs/${type}.err" classify_concept_sbatch.bash $command
+	sbatch --job-name="${type}" --output="logs/${type}.out" --error="logs/${type}.err" classify_concept_sbatch.bash "${command}"
 done
