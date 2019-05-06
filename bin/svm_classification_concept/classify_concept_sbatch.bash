9528bb28b6b07d066158afa95f14e228973481a8
Angus Leung <prototype032@gmail.com>
2019-05-06 14:23:13 +1000

    increased max walltime

--- bin/svm_classification_concept/classify_concept_sbatch.bash
+++ bin/svm_classification_concept/classify_concept_sbatch.bash
@@ -24,7 +24,7 @@
 #SBATCH --mem-per-cpu=8000
 
 # Set your minimum acceptable walltime, format: day-hours:minutes:seconds
-#SBATCH --time=0-3:00:00
+#SBATCH --time=0-10:00:00
 
 # SBATCH --qos=shortq
 
