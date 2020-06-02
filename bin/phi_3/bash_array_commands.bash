c12c6c2d8b671f671e0c8ce0bd95bde748012fb3
Angus Leung <prototype032@gmail.com>
2020-06-02 18:23:27 +1000

    updated to use binning average for timescale

--- bin/phi_3/bash_array_commands.bash
+++ bin/phi_3/bash_array_commands.bash
@@ -7,8 +7,8 @@ set_ids=($(seq 1 1 1))
 taus=(1 2 3 4 5 10 20 30 40 50 75 100 125 150 175 200 225 250) #(4) #(1 2 3 4 8 12 16 24 32 48 64 128 256 512 4500 9000) #(4 8 16)
 trials=8
 global_tpm=0
-tau_bin=0
-sample_offset=0
+tau_bin=1
+sample_offset=1
 > array_commands
 for (( fly=1; fly<=$flies; fly++ )); do
 	for condition in "${conditions[@]}"; do
