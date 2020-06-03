465865122cac157ec087672f08d2240f573ab237
Angus Leung <prototype032@gmail.com>
2020-06-03 22:26:03 +1000

    for tau-bin method

--- bin/phi_3/main_tauCompare.m
+++ bin/phi_3/main_tauCompare.m
@@ -6,7 +6,8 @@ Plots big-phi at each tau level
 
 %% Setup
 
-load('results/split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim_ICAAllTrials_nComponents4_phithree_nChannels4_globalTPM0.mat');
+%load('results/split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim_ICAAllTrials_nComponents4_phithree_nChannels4_globalTPM0.mat');
+load('results/split2250_bipolarRerefType1_lineNoiseRemoved_postPuffpreStim_ICAAllTrials_nComponents4_phithree_nChannels4_globalTPM0_binOffset1.mat');
 
 %% Plot big-phi
 % 1 plot per fly, comparing all trials of wake vs anesthesia
@@ -26,7 +27,8 @@ for fly = 1 : size(big_phis, 3)
         plot(phis{1}.taus, squeeze(mean(big_phis(:, :, fly, condition, :), 2)), cond_colours{condition}, 'LineWidth', 3);
     end
     set(gca, 'XScale', 'log');
-    xlabel('tau-step (ms)');
+    set(gca, 'YScale', 'log');
+    xlabel('\tau (ms)');
     ylabel('\Phi');
     title(['fly' num2str(fly)]);
     
@@ -43,7 +45,8 @@ for condition = 1 : size(big_phis, 4)
     plot(phis{1}.taus, squeeze(mean(mean(big_phis(:, :, :, condition, :), 2), 3)), cond_colours{condition}, 'LineWidth', 3);
 end
 set(gca, 'XScale', 'log');
-xlabel('tau-step (ms)');
+set(gca, 'YScale', 'log');
+xlabel('\tau (ms)');
 ylabel('\Phi');
 title(['all flies']);
 
@@ -52,7 +55,7 @@ title(['all flies']);
 figure;
 for fly = 1 : size(big_phis, 3)
     subplot(4, 4, fly);
-    data = big_phis(:, :, fly, 1, :) - big_phis(:, :, fly, 2, :);
+    data = big_phis(:, :, fly, 1, :) ./ big_phis(:, :, fly, 2, :);
     % Plot individual trials
     plot(phis{1}.taus, squeeze(data), 'k-.');
     hold on;
@@ -60,20 +63,20 @@ for fly = 1 : size(big_phis, 3)
     plot(phis{1}.taus, squeeze(mean(data, 2)), 'k', 'LineWidth', 3);
     
     set(gca, 'XScale', 'log');
-    xlabel('tau-step (ms)');
+    xlabel('tau (ms)');
     ylabel('\Delta\Phi');
     title(['fly' num2str(fly)]);
 end
 
 % Population (all flies)
 subplot(4, 4, fly+1)
-data = big_phis(:, :, :, 1, :) - big_phis(:, :, :, 2, :);
+data = big_phis(:, :, :, 1, :) ./ big_phis(:, :, :, 2, :);
 % Plot individuals flies
 plot(phis{1}.taus, squeeze(mean(data, 2)), 'k-.');
 hold on;
 % Plot average across flies
 plot(phis{1}.taus, squeeze(mean(mean(data, 2), 3)), 'k', 'LineWidth', 3);
 set(gca, 'XScale', 'log');
-xlabel('tau-step (ms)');
+xlabel('\tau (ms)');
 ylabel('\Delta\Phi');
 title(['all flies']);
